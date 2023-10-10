1514d774722833f04ba07419bd5db963c401337b
Will Yi <yiwill07@gmail.com>
2023-10-10 16:00:05 -0400

    Added some base files
    How exciting

---
+++ main.cpp
@@ -0,0 +1,6 @@
+#include <one_sample_t_test_means>
+#include <iostream>
+
+int main () {
+    std::cout << "Hello world";
+}
---
+++ one_sample_z_proportions.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+int main () {
+    std::cout << "To be finished";
+}
